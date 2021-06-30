cbeabea6431fbb17893a528d5ea6f84ee6dfa1e7
Nick Harper <bignickharper@gmail.com>
2021-06-30 15:28:10 -0700

    folder creator

---
+++ Utilities/Folder Creator/folderCreator.py
@@ -0,0 +1,20 @@
+from ntpath import join
+import os
+
+folderList = open(
+    join(os.getcwd(), "Utilities/Folder Creator/folderNames.txt"), "r")
+
+folders = [line.split() for line in folderList]
+
+
+consolidated = []
+
+for names in folders:
+    ready = '-'.join(names)
+    consolidated.append(ready)
+
+for path in consolidated:
+    os.mkdir(path.lower())
+
+# for path in consolidated:
+#     os.mkdir(path.lower())
